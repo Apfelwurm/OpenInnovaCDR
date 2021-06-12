5493cbad637e4fe58922c38978cffaea3c7d11b9
Alexander Volz <Alexander@volzit.de>
2021-06-13 01:05:03 +0200

    changed datacollector to post

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::put('organisationunit/{id}', 'App\Http\Controllers\Api\OrganisationUnitsC
 Route::delete('organisationunit/{id}', 'App\Http\Controllers\Api\OrganisationUnitsController@delete');
 
 
-Route::get('datacollector/innovaphone', 'App\Http\Controllers\Api\DataCollectionController@innovaphone');
+Route::post('datacollector/innovaphone', 'App\Http\Controllers\Api\DataCollectionController@innovaphone');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
