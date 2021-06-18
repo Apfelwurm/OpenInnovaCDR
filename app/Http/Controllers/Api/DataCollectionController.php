24f9bf2202a363d8f2617d896275944ffe61245f
Alexander Volz <Alexander@volzit.de>
2021-06-18 15:49:10 +0200

    fix phonecall selection

--- app/Http/Controllers/Api/DataCollectionController.php
+++ app/Http/Controllers/Api/DataCollectionController.php
@@ -195,7 +195,7 @@ class DataCollectionController extends Controller
             Storage::disk('local')->append('innovaphonerequestlog.txt', json_encode($phonecall));
 
 
-            if ($phonecall->dir == "from" && $phonecall->e164 != null && $phonecall->e164 != "" && (($firstcallevent->type != "ext" && !(str_starts_with($phonecall->e164, "00")) ) || ($firstcallevent->type == "ext" && str_starts_with($phonecall->e164, "00") ) ))
+            if ($phonecall->dir == "from" && $phonecall->e164 != null && $phonecall->e164 != "" && (($firstcallevent->type != "ext" && !(str_starts_with($firstcallevent->e164, "00")) ) || ($firstcallevent->type == "ext" && str_starts_with($firstcallevent->e164, "00") ) ))
                     {
                         Storage::disk('local')->append('innovaphonerequestlog.txt', "phonecall selected");
                         Storage::disk('local')->append('innovaphonerequestlog.txt', "number:");
